69b5b5240bc8e4469d70ad2a37de967bd4982267
akhamat cool <newbie.hao.semarang@gmail.com>
2018-07-15 21:05:05 +0700

    commit all change

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,21 +9,21 @@
 
     <!-- Font -->
 
-    <link href="http://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
 
 
     <!-- Stylesheets -->
 
-    <link href="{{ asset('common-css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('common-css/bootstrap.css') }}" rel="stylesheet">
 
-    <link href="{{ asset('common-css/swiper.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('common-css/swiper.css') }}" rel="stylesheet">
 
-    <link href="{{ asset('common-css/ionicons.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('common-css/ionicons.css') }}" rel="stylesheet">
 
 
-    <link href="{{ asset('front-page-category/css/styles.css?v=1') }}" rel="stylesheet">
+    <link href="{{ secure_asset('front-page-category/css/styles.css?v=1') }}" rel="stylesheet">
 
-    <link href="{{ asset('front-page-category/css/responsive.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('front-page-category/css/responsive.css') }}" rel="stylesheet">
 
 </head>
 <body >
@@ -33,17 +33,17 @@
 
     <!-- SCIPTS -->
 
-    <script src="{{ asset('common-js/jquery-3.1.1.min.js') }}"></script>
+    <script src="{{ secure_asset('common-js/jquery-3.1.1.min.js') }}"></script>
 
-    <script src="{{ asset('common-js/tether.min.js') }}"></script>
+    <script src="{{ secure_asset('common-js/tether.min.js') }}"></script>
 
-    <script src="{{ asset('common-js/bootstrap.js') }}"></script>
+    <script src="{{ secure_asset('common-js/bootstrap.js') }}"></script>
 
-    <script src="{{ asset('common-js/swiper.js') }}"></script>
+    <script src="{{ secure_asset('common-js/swiper.js') }}"></script>
 
-    <script src="{{ asset('common-js/scripts.js') }}"></script>
+    <script src="{{ secure_asset('common-js/scripts.js') }}"></script>
 
-    <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
+    <script src="{{ secure_asset('ckeditor/ckeditor.js') }}"></script>
 
 
      @yield('footer-script')
